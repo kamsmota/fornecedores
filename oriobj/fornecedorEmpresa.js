cac229f7f3a2a141a244c305cc6ee5005ef3fa3a
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-20 20:52:53 -0300

    Delete oriobj directory

--- oriobj/fornecedorEmpresa.js
+++
@@ -1,7 +0,0 @@
-import Fornecedor from "./fornecedor"
-import FornecedorPessoa from "./fornecedorPessoa"
-
-
-const fornecedor = new Fornecedor("Pedro", "(11) 12345-6789")
-
-console.log("dados do fornecedor:")
